d96280174d281368d61c8fb73cdf3577690fae1e
N3xiZ <pascal.krain@yahoo.com>
2022-05-30 16:26:06 +0200

    integrated user editable config.yml

--- src/main/java/eu/pkdb/restartplugin/RestartPlugin.java
+++ src/main/java/eu/pkdb/restartplugin/RestartPlugin.java
@@ -7,21 +7,31 @@ import eu.pkdb.restartplugin.utils.ConsoleUtil;
 import eu.pkdb.restartplugin.utils.Constants;
 import eu.pkdb.restartplugin.utils.DateUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.logging.Level;
 
 public final class RestartPlugin extends JavaPlugin {
+    private File customConfigFile;
+    private FileConfiguration customConfig;
 
     @Override
     public void onEnable() {
+        initStartup();
+
         LocalDateTime now = LocalDateTime.now();
 
-        //TODO: read hours, mins, secs from yaml file
-        LocalDateTime restartTime = now.plusHours(4);
+        LocalDateTime restartTime = now.plusHours(this.customConfig.isSet(Constants.HOURS_CFG) ? this.customConfig.getLong(Constants.HOURS_CFG) : 4);
+        restartTime = restartTime.plusMinutes(this.customConfig.isSet(Constants.MINS_CFG) ? this.customConfig.getLong(Constants.MINS_CFG) : 0);
+        restartTime = restartTime.plusSeconds(this.customConfig.isSet(Constants.SECS_CFG) ? this.customConfig.getLong(Constants.SECS_CFG) : 0);
 
         Bukkit.getLogger().log(Level.INFO, Constants.LOGGER_NAME + " Next server restart -> " + restartTime);
 
@@ -32,7 +42,10 @@ public final class RestartPlugin extends JavaPlugin {
 
         Timer timer2 = new Timer();
         timer2.schedule(new RestartCounter(this, "15"), DateUtil.convertToDateViaInstant(restartTime.minusSeconds(15)));
+    }
 
+    private void initStartup() {
+        createCustomConfig();
     }
 
     @Override
@@ -50,5 +63,39 @@ public final class RestartPlugin extends JavaPlugin {
 
     public static void counter(String seconds) {
         ConsoleUtil.toColsole("restart in " + seconds + " seconds");
+    }
+
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
+    }
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "config.yml");
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            try {
+                customConfigFile.createNewFile();
+            } catch (IOException e) {
+                Bukkit.getLogger().log(Level.SEVERE, e.getMessage());
+            }
+        }
+
+        customConfig = new YamlConfiguration();
+
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            Bukkit.getLogger().log(Level.SEVERE, e.getMessage());
+        }
+        // Setting up default values if not set
+        if(!customConfig.isSet(Constants.HOURS_CFG)) customConfig.set(Constants.HOURS_CFG, 4);
+        if(!customConfig.isSet(Constants.MINS_CFG)) customConfig.set(Constants.MINS_CFG, 0);
+        if(!customConfig.isSet(Constants.SECS_CFG)) customConfig.set(Constants.SECS_CFG, 0);
+
+        try {
+            customConfig.save(customConfigFile);
+        } catch (IOException e) {
+            Bukkit.getLogger().log(Level.SEVERE, e.getMessage());
+        }
     }
 }
--- src/main/java/eu/pkdb/restartplugin/utils/Constants.java
+++ src/main/java/eu/pkdb/restartplugin/utils/Constants.java
@@ -2,4 +2,9 @@ package eu.pkdb.restartplugin.utils;
 
 public class Constants {
     public static final String LOGGER_NAME = "[RestartPlugin]";
+
+    // config.yml
+    public static final String HOURS_CFG = "hours";
+    public static final String MINS_CFG = "minutes";
+    public static final String SECS_CFG = "seconds";
 }
